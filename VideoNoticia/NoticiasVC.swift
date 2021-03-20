762f839dbd5068dad4f6526e14d1a3b2a61137f5
Diego Alberto Osorio Zendejas <diego.osorio.az@gmail.com>
2021-03-20 16:41:17 -0600

    Initial Commit

---
+++ VideoNoticia/NoticiasVC.swift
@@ -0,0 +1,44 @@
+//
+//  NoticiasVC.swift
+//  VideoNoticia
+//
+//  Created by alicharlie on 12/05/16.
+//  Copyright © 2016 codepix. All rights reserved.
+//
+
+import UIKit
+
+
+class NoticiasVC: UITableViewController {
+
+    var noticias:[String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        LeerNoticias().getNoticias { (datos) in
+              self.noticias = datos
+              self.tableView.reloadData()
+        }
+        
+        
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.noticias.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celda", for: indexPath as IndexPath)
+        
+        celda.textLabel!.text = self.noticias[indexPath.row]
+        
+        return celda
+        
+    }
+    
+    
+}
